afe163c1664b19646ce39bce568b841c0da4b0df
Abhik Lodh <88699987+Abhik-67@users.noreply.github.com>
2021-09-11 16:29:12 +0530

    Add files via upload

---
+++ User input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int age,salary;
+	cout<<"Enter age and salary:\n";
+	cin>>age>>salary;
+	cout<<"The age is:\n"<<age;
+	cout<<"\nThe salary is:\n"<<salary;
+	return 0;
+}
